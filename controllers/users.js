04a7df53ca98acc0ff32d652e421008401489e4d
Abin <abin@luminescent.digital>
2020-06-29 15:26:22 +0530

    Added validation usin g joi

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 module.exports = {
 	signUp: async (req, res, next) => {
 		console.log("sign up");
+		console.log("body", req.value.body);
 	},
 
 	signIn: async (req, res, next) => {
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = require('express-promise-router')();
 
 const UserControler = require('../controllers/users');
+const { validateBody, schemas } = require('../helpers/routerHelper');
 
-router.route('/signup').post(UserControler.signUp);
+router.route('/signup')
+.post(validateBody(schemas.authSchema),UserControler.signUp);
 
 router.route('/signin').post(UserControler.signIn);
 
